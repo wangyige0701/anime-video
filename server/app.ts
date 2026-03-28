befd8a04b1c30bb273beba8cd8c135345d9dbeb4
wangyige0701 <1776686515@qq.com>
2026-03-28 21:49:44 +0800

    升级路由插件版本

--- server/app.ts
+++ server/app.ts
@@ -1,20 +1,16 @@
 import Koa from 'koa';
 import body from 'koa-body';
-import { decorator } from 'koa-use-decorator-route';
+import Decorator from 'koa-use-decorator-route';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { response } from '@server/koa/response';
 
-const app = new Koa();
+const dir = resolve(dirname(fileURLToPath(import.meta.url)), './controller');
 
-app.use(body())
-	.use(response())
-	.use(
-		decorator({
-			controllerDir: resolve(dirname(fileURLToPath(import.meta.url)), './controller'),
-			allowedMethods: true,
-		}),
-	);
+const app = new Koa();
+const decorator = new Decorator(dir);
+
+app.use(body()).use(response()).use(decorator.middleware()).use(decorator.allowedMethods());
 
 app.listen(3000, () => {
 	console.log('server is running on http://localhost:3000');

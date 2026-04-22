0d824b4a2a608a4723a9222db641ebfa80a74745
wangyige0701 <1776686515@qq.com>
2026-04-22 23:44:41 +0800

    列表页初步样式

--- web/src/components/IndexVideoItem.vue
+++ web/src/components/IndexVideoItem.vue
@@ -1,17 +1,23 @@
 <template>
-	<div class="index-video-item">
+	<div class="index-video-item" @click="gotoDetail(item)">
 		<div class="index-video-img-container">
 			<Image :src="imagePath" class="index-video-img"></Image>
+		</div>
+
+		<div class="index-video-name">
+			<span>{{ item.name }}</span>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import type { Series } from '~types/videos';
+import { computed } from 'vue';
+import { getImageUrl } from '~routes/server';
+import { WebRoute } from '~routes/web';
 import Image from '@/components/Image.vue';
 import { getSeriesPath } from '@/utils/series';
-import { computed } from 'vue';
-import { getImageUrl } from '@config/route';
+import router from '@/router';
 
 const props = defineProps<{ item: Series }>();
 
@@ -21,26 +27,49 @@ const imagePath = computed(() => {
 	}
 	return 'http://localhost:3000' + getImageUrl(getSeriesPath(props.item.rootPath, props.item.images[0]!));
 });
+
+function gotoDetail(item: Series) {
+	router.push({ name: WebRoute.DETAIL, params: { name: item.name } });
+}
 </script>
 
 <style scoped lang="scss">
 .index-video-item {
+	--radius: 8px;
+	--padding: 5px;
+
 	cursor: pointer;
 	width: var(--index-video-item-width);
 	display: flex;
 	flex-direction: column;
-	padding: 5px;
+	padding: var(--padding);
 	background-color: var(--index-video-item-background-color);
-	border-radius: 10px;
+	border-radius: var(--radius);
+	transition: box-shadow 0.3s ease;
+
+	&:hover {
+		box-shadow: 0 0 0px 6px var(--index-video-item-background-color);
+	}
 }
 
 .index-video-img-container {
 	width: 100%;
+
+	.index-video-img {
+		width: 100%;
+		object-fit: cover;
+		border-radius: calc(var(--radius) - var(--padding) / 2);
+	}
 }
 
-.index-video-img-container .index-video-img {
-	width: 100%;
-	object-fit: cover;
-	border-radius: 10px;
+.index-video-name {
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	flex: 1;
+	font-size: 0.875rem;
+	color: var(--index-video-item-name-color);
+	text-decoration: none;
+	padding: 0.5em;
 }
 </style>

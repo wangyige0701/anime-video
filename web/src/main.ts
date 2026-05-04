2367053ddf340adf654c3111c2b187560b60b058
wangyige0701 <1776686515@qq.com>
2026-05-05 02:15:43 +0800

    样式文件调整

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import '@/assets/style.css';
+import '@/assets/style.scss';
 import '@/assets/iconfont.css';
 
 import App from './App.vue';
